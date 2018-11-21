8ccd19ce7b1e70b837cfe621b4d167deeefebacc
jtokib <tokib520@yahoo.com>
2018-11-20 22:56:39 -0800

    Updates

---
+++ precache-manifest.370bc46c6205412309dabb34ee17a44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9de2fedb23a6810f103d",
+    "url": "/react-file/static/js/runtime~main.9de2fedb.js"
+  },
+  {
+    "revision": "aa69013d54eb9749ea3c",
+    "url": "/react-file/static/js/main.aa69013d.chunk.js"
+  },
+  {
+    "revision": "27e6a19419f8aab2a38e",
+    "url": "/react-file/static/js/1.27e6a194.chunk.js"
+  },
+  {
+    "revision": "aa69013d54eb9749ea3c",
+    "url": "/react-file/static/css/main.26ab4474.chunk.css"
+  },
+  {
+    "revision": "7949ac3a9a748ecf5e300134bd2a67b9",
+    "url": "/react-file/index.html"
+  }
+];

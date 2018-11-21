bef79156d770ceeb2948cb510e1fffc739c6f457
jtokib <tokib520@yahoo.com>
2018-11-20 23:05:30 -0800

    Updates

---
+++ precache-manifest.a3740920e219b7777c1c20aafbe64985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74e9391f5e29eaee5c1f",
+    "url": "/static/js/main.74e9391f.chunk.js"
+  },
+  {
+    "revision": "27e6a19419f8aab2a38e",
+    "url": "/static/js/1.27e6a194.chunk.js"
+  },
+  {
+    "revision": "74e9391f5e29eaee5c1f",
+    "url": "/static/css/main.26ab4474.chunk.css"
+  },
+  {
+    "revision": "f8845998f13d254680a62e4bcacd85a3",
+    "url": "/index.html"
+  }
+];

8983099260374fbb94fcb0a59fbf99d27007a7ba
jtokib <tokib520@yahoo.com>
2018-11-27 00:16:03 -0800

    Updates

--- precache-manifest.d1aec0b49b13264b394b6bfef7bc4369.js
+++ precache-manifest.d1aec0b49b13264b394b6bfef7bc4369.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-file/static/js/runtime~main.9de2fedb.js"
   },
   {
-    "revision": "9da71a7deed043e428c2",
-    "url": "/react-file/static/js/main.9da71a7d.chunk.js"
+    "revision": "b3d1825e9185d871a7d5",
+    "url": "/react-file/static/js/main.b3d1825e.chunk.js"
   },
   {
     "revision": "27e6a19419f8aab2a38e",
     "url": "/react-file/static/js/1.27e6a194.chunk.js"
   },
   {
-    "revision": "9da71a7deed043e428c2",
+    "revision": "b3d1825e9185d871a7d5",
     "url": "/react-file/static/css/main.26ab4474.chunk.css"
   },
   {
-    "revision": "a16d0014ce78c4cc1829f4718617b865",
+    "revision": "becee2e7e1b0b25643de84469baebb49",
     "url": "/react-file/index.html"
   }
 ];
